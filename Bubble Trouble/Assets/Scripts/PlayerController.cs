9f7e813ac56c2e7e64252272493e4f57e33207ca
Tanner <tannercobb359@gmail.com>
2021-03-17 20:40:46 -0400

    Minor Tilt and movement speed adjustment

--- Bubble Trouble/Assets/Scripts/PlayerController.cs
+++ Bubble Trouble/Assets/Scripts/PlayerController.cs
@@ -18,12 +18,19 @@ public class PlayerController : MonoBehaviour
         tilt = Input.acceleration;
         Debug.Log(tilt);
         Walk(tilt.x);
+        
+    }
+
+    private void Update()
+    {
         if (Input.touchCount > 0)
         {
             touch = Input.GetTouch(0);
-            switch(touch.phase)
+            switch (touch.phase)
             {
-                case TouchPhase.Began :
+                default:
+                    break;
+                case TouchPhase.Began:
                     Swing();
                     break;
             }
@@ -49,18 +56,19 @@ public class PlayerController : MonoBehaviour
 
     public void Walk(float direction)
     {
-        if (direction <= -0.25f)
+        if (direction <= -0.10f)
         {
             animator.SetBool("Walk", true);
-            rb.velocity = Vector2.left * movementSpeed;
+            rb.velocity = Vector2.left * movementSpeed * Mathf.Abs(direction);
         }
-        else if (direction >= 0.25f)
+        else if (direction >= 0.10f)
         {
             animator.SetBool("Walk", true);
-            rb.velocity = Vector2.right * movementSpeed;
+            rb.velocity = Vector2.right * movementSpeed * Mathf.Abs(direction);
         }
         else
         {
+            animator.SetBool("Walk", false);
             rb.velocity = Vector2.zero;
         }
     }
