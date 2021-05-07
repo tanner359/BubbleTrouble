301ba74bb0e04287dda9754d1a05a3ffd6138673
Tanner Cobb <tannercobb359@gmail.com>
2021-05-06 22:12:19 -0400

    Fixed various scaling issues with enemy AI

--- Bubble Trouble/Assets/Scripts/Enemies/Enemy.cs
+++ Bubble Trouble/Assets/Scripts/Enemies/Enemy.cs
@@ -21,15 +21,17 @@ public class Enemy : MonoBehaviour
    
     [Space(5)]
     [Header("Boundry Modifiers")]
-    public float MIN_X = 0;
-    public float MAX_X = 0;
-    public float MIN_Y = 0;
-    public float MAX_Y = 0;
-   
-    Vector2 Bottom_L;
-    Vector2 Top_L;
-    Vector2 Bottom_R;
-    Vector2 Top_R;
+    public Vector2 Bottom_L;
+    Vector2 BL_Offset;
+    public Vector2 Top_L;
+    Vector2 TL_Offset;
+    public Vector2 Bottom_R;
+    Vector2 BR_Offset;
+    public Vector2 Top_R;
+    Vector2 TR_Offset;
+
+    Vector2 leftAnchorStart;
+    Vector2 rightAnchorStart;
 
     [Space(10)]
     [Header("AI Movement")]
@@ -55,11 +57,19 @@ public class Enemy : MonoBehaviour
 
     private void Start()
     {
+        BL_Offset = Bottom_L;
+        TL_Offset = Top_L;
+        BR_Offset = Bottom_R;
+        TR_Offset = Top_R;
+
+        leftAnchorStart = Camera.main.ScreenToWorldPoint(new Vector3(0, 2400f / 2, Camera.main.transform.position.z));
+        rightAnchorStart = Camera.main.ScreenToWorldPoint(new Vector3(1080f, 2400f / 2, Camera.main.transform.position.z));
+
         player = GameObject.FindGameObjectWithTag("Player");
         renderers = body.GetComponentsInChildren<SpriteRenderer>();
         rb = GetComponent<Rigidbody2D>();
 
-        if (IsInsideBounds())
+        if (IsInsideBounds(transform.position))
         {
             StartCoroutine(Wander());
         }
@@ -76,16 +86,18 @@ public class Enemy : MonoBehaviour
 
     public void Update()
     {
-        #region BOUNDRY POSITION CALCULATIONS
-        Bottom_L = Camera.main.ScreenToWorldPoint(new Vector3(0 + MIN_X, 0 + MIN_Y, Camera.main.transform.position.z));
-        Top_L = Camera.main.ScreenToWorldPoint(new Vector3(0 + MIN_X, Screen.height + MAX_Y, Camera.main.transform.position.z));
-        Bottom_R = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width + MAX_X, 0 + MIN_Y, Camera.main.transform.position.z));
-        Top_R = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width + MAX_X, Screen.height + MAX_Y, Camera.main.transform.position.z));
+        #region BOUNDRY SCREEN SCALE
+        Vector2 leftAnchor = Camera.main.ScreenToWorldPoint(new Vector3(0, Screen.height / 2, Camera.main.transform.position.z));
+        Vector2 rightAnchor = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height / 2, Camera.main.transform.position.z));
 
-        Debug.DrawLine(Bottom_L, Bottom_R, Color.red);
-        Debug.DrawLine(Bottom_R, Top_R, Color.red);
-        Debug.DrawLine(Top_R, Top_L, Color.red);
-        Debug.DrawLine(Bottom_L, Top_L, Color.red);
+        Bottom_L =  new Vector2(leftAnchor.x + (BL_Offset.x - leftAnchorStart.x), BL_Offset.y);
+
+        Bottom_R = new Vector2(rightAnchor.x + (BR_Offset.x - rightAnchorStart.x), BR_Offset.y);
+
+        Top_L = new Vector2(leftAnchor.x + (TL_Offset.x - leftAnchorStart.x), TL_Offset.y);
+
+        Top_R = new Vector2(rightAnchor.x + (TR_Offset.x - rightAnchorStart.x), TR_Offset.y);
+
         #endregion
 
         #region UPDATE LOCATIONS RELATIVE POSITION
@@ -112,7 +124,7 @@ public class Enemy : MonoBehaviour
         if (attack || wander || move)
         {
             transform.position = Vector2.MoveTowards(transform.position, target, Time.deltaTime * movementSpeed);
-            if (!IsInsideBounds() && !attack && !move)
+            if (!IsInsideBounds(transform.position) && !attack && !move)
             {
                 StopAllCoroutines();
                 move = true;
@@ -170,7 +182,7 @@ public class Enemy : MonoBehaviour
         movementSpeed = 10f;
         target = GeneratePointInBounds();
         move = true;     
-        yield return new WaitUntil(() => IsInsideBounds());
+        yield return new WaitUntil(() => IsInsideBounds(transform.position));
         yield return new WaitForSeconds(0.2f);
         move = false;
         StartCoroutine(Wander());
@@ -222,15 +234,49 @@ public class Enemy : MonoBehaviour
     {
         return Random.Range(MIN, MAX);
     }
-    public bool IsInsideBounds()
+    public bool IsInsideBounds(Vector2 pos)
     {
         float posX = transform.position.x, posY = transform.position.y;       
-        if(posX >= Bottom_L.x && posX <= Bottom_R.x && posY >= Bottom_L.y && posY <= Top_L.y) {return true;}
+        if(posX >= Bottom_L.x && posX <= Bottom_R.x && posX <= Top_R.x && posX >= Top_L.x 
+        && posY >= Bottom_L.y && posY >= Bottom_R.y && posY <= Top_L.y && posY <= Top_R.y) {return true;}
         else{return false;}
     }
     public Vector2 GeneratePointInBounds()
     {
-        return new Vector2(Random.Range(Bottom_L.x, Bottom_R.x), Random.Range(Bottom_L.y, Top_L.y));
+        float x1 = Random.Range(Bottom_L.x, Top_L.x);
+        float m1 = (Top_L.y - Bottom_L.y) / (Top_L.x - Bottom_L.x);
+        float b1 = -1*(m1 * Bottom_L.x) + Bottom_L.y;
+        Vector2 Left = new Vector2(x1, m1 * x1 + b1);
+
+        float x2 = Random.Range(Top_L.x, Top_R.x);
+        float m2 = (Top_R.y - Top_L.y) / (Top_R.x - Top_L.x);
+        float b2 = -1 * (m2 * Top_L.x) + Top_L.y;
+        Vector2 Top = new Vector2(x2, m2 * x2 + b2);
+
+        float x3 = Random.Range(Bottom_R.x, Top_R.x);
+        float m3 = (Bottom_R.y - Top_R.y) / (Bottom_R.x - Top_R.x);
+        float b3 = -1 * (m3 * Bottom_R.x) + Bottom_R.y;
+        Vector2 Right = new Vector2(x3, m3 * x3 + b3);
+
+        float x4 = Random.Range(Bottom_L.x, Bottom_R.x);
+        float m4 = (Bottom_R.y - Bottom_L.y) / (Bottom_R.x - Bottom_L.x);
+        float b4 = -1 * (m4 * Bottom_L.x) + Bottom_L.y;
+        Vector2 Bottom = new Vector2(x4, m4 * x4 + b4);
+
+        float x5 = 0;
+        float m5 = (Right.y - Left.y) / (Right.x - Left.x);
+        float b5 = -1 * (m5 * Left.x) + Left.y;
+        float Line1 = m5 * x5 + b5;
+
+        float x6 = 0;
+        float m6 = (Top.y - Bottom.y) / (Top.x - Bottom.x);
+        float b6 = -1 * (m6 * Bottom.x) + Bottom.y;
+        float Line2 = m6 * x6 + b6;
+
+        float final_X = (b6 - b5) / (m5 - m6);
+        float final_Y = m6 * final_X + b6;
+
+        return new Vector2(final_X, final_Y);
     }
     #endregion
 
